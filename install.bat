94a45f9865de442d1116eb1c2e5b8710aae055da
Stephen Merendino <stephenmerendino@gmail.com>
2016-04-07 22:42:08 -0500

    Moved to submodules

--- install.bat
+++ install.bat
@@ -9,9 +9,4 @@ REM Make sure those files source the actual files here
 echo source %cur_dir%_vimrc > %USERPROFILE%\_vimrc
 echo source %cur_dir%_gvimrc > %USERPROFILE%\_gvimrc
 
-REM Put pathogen in autoload then cleanup
-git clone https://github.com/tpope/vim-pathogen
-copy vim-pathogen\autoload\pathogen.vim autoload\
-rmdir /Q /S vim-pathogen
-
 echo ...DONE!
