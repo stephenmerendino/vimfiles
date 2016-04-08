2aba58c164c76cb1dc8b72a15801cd03187c0565
Stephen Merendino <stephenmerendino@gmail.com>
2016-04-07 22:39:13 -0500

    Changed to be a more standard setup. Drop it in and run a quick script to init everything

--- install.bat
+++ install.bat
@@ -9,22 +9,9 @@ REM Make sure those files source the actual files here
 echo source %cur_dir%_vimrc > %USERPROFILE%\_vimrc
 echo source %cur_dir%_gvimrc > %USERPROFILE%\_gvimrc
 
-REM Setup vimfiles folder in %USERPROFILE%
-if not exist %USERPROFILE%\vimfiles mkdir %USERPROFILE%\vimfiles
-if not exist %USERPROFILE%\vimfiles\autoload mkdir %USERPROFILE%\vimfiles\autoload
-if not exist %USERPROFILE%\vimfiles\bundle mkdir %USERPROFILE%\vimfiles\bundle
-
-set vim_path=%USERPROFILE%\vimfiles
-
 REM Put pathogen in autoload then cleanup
 git clone https://github.com/tpope/vim-pathogen
-copy vim-pathogen\autoload\pathogen.vim %vim_path%\autoload\
+copy vim-pathogen\autoload\pathogen.vim autoload\
 rmdir /Q /S vim-pathogen
-
-REM Put plugins in bundle
-pushd %vim_path%\bundle\
-git clone https://github.com/scrooloose/nerdtree
-git clone https://github.com/kien/ctrlp.vim
-popd
 
 echo ...DONE!
